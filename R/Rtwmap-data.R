d709d254917c1e01c569057c284826c474b9279e
Wush Wu <wush978@gmail.com>
2012-12-07 00:42:29 +0800

    finish data description

--- R/Rtwmap-data.R
+++ R/Rtwmap-data.R
@@ -1,8 +1,44 @@
 #' @name county1984
 #' @title SpatialPolygonsDataFrame of Taiwan maps
 #' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @usage data(county1984)
 #' @docType data
 #' @references https://github.com/ronnywang/twmap
-#' @references  
-#' @format 5 \code{SpatialPolygonsDataFrame} instances
+#' @format \code{SpatialPolygonsDataFrame} instances
+NULL
+
+#' @name county2010
+#' @title SpatialPolygonsDataFrame of Taiwan maps
+#' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @usage data(county2010)
+#' @docType data
+#' @references https://github.com/ronnywang/twmap
+#' @format \code{SpatialPolygonsDataFrame} instances
+NULL
+
+#' @name town1984
+#' @title SpatialPolygonsDataFrame of Taiwan maps
+#' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @usage data(town1984)
+#' @docType data
+#' @references https://github.com/ronnywang/twmap
+#' @format \code{SpatialPolygonsDataFrame} instances
+NULL
+
+#' @name town2010
+#' @title SpatialPolygonsDataFrame of Taiwan maps
+#' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @usage data(town2010)
+#' @docType data
+#' @references https://github.com/ronnywang/twmap
+#' @format \code{SpatialPolygonsDataFrame} instances
+NULL
+
+#' @name village2010
+#' @title SpatialPolygonsDataFrame of Taiwan maps
+#' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @usage data(village2010)
+#' @docType data
+#' @references https://github.com/ronnywang/twmap
+#' @format \code{SpatialPolygonsDataFrame} instances
 NULL
