d1980a59b08e3955e1ea2edc138e47b602e51f0f
Wush Wu <wush978@gmail.com>
2012-12-07 00:34:36 +0800

    init

---
+++ R/Rtwmap-data.R
@@ -0,0 +1,8 @@
+#' @name county1984
+#' @title SpatialPolygonsDataFrame of Taiwan maps
+#' @description This dataset contains the map of county, town, and village of Taiwan.
+#' @docType data
+#' @references https://github.com/ronnywang/twmap
+#' @references  
+#' @format 5 \code{SpatialPolygonsDataFrame} instances
+NULL
